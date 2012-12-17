d0489da42c1cd7766fb1b51376ea59b37de40f34
Tristan Seifert <tristan@squee.co>
2012-12-16 18:07:41 -0600

    Added MPLAB X project for PIC firmware.

---
+++ Software/VariablePSU_FW.X/build/XC8_PIC18F4550/production/interrupts.p1.d
@@ -0,0 +1,3 @@
+ build/XC8_PIC18F4550/production/interrupts.d  \
+ build/XC8_PIC18F4550/production/interrupts.p1:  \
+ interrupts.c 
---
+++ Software/VariablePSU_FW.X/build/XC8_PIC18F4550/production/system.p1.d
@@ -0,0 +1,4 @@
+ build/XC8_PIC18F4550/production/system.d  \
+ build/XC8_PIC18F4550/production/system.p1:  \
+ system.c  \
+system.h 
